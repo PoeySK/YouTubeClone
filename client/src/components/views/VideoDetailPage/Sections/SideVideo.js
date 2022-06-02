37fa70ef58d965ff0b7e33d5ea672316e130b1ac
YeopMan <201802044@o.cnu.ac.kr>
2022-06-03 00:57:11 +0900

    To Add the keyword 'key' to map

--- client/src/components/views/VideoDetailPage/Sections/SideVideo.js
+++ client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -15,11 +15,11 @@ function SideVideo() {
             })
     }, [])
 
-    const renderSideVideo = sideVideos.map((video, index) => {
+    const renderSideVideo = sideVideos.map((video) => {
         let minutes = Math.floor(video.duration / 60);
         let seconds = Math.floor(video.duration - minutes * 60);
         return (
-            <div style={{ display: 'flex', marginBottom: "1rem", padding: "0 2rem" }} key={index}>
+            <div style={{ display: 'flex', marginBottom: "1rem", padding: "0 2rem" }} key={video._id}>
                 <div style={{ width: '40%', marginRight: "1rem" }}>
                     <a href=''>
                         <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
